8018d45faeffa06250801cbcff2d8e3beaba4ac5
Luffy D <elmonkeyd.luffyrandom@gmail.com>
2025-11-07 01:57:52 +0000

    sigue en blanco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { AppProvider } from '@/contexts/app-context';
 import { PageClient } from './page-client';
-import { FirebaseProvider } from '@/firebase/provider';
 
 export default function Home() {
   return (
-    <FirebaseProvider>
-      <AppProvider>
-        <PageClient />
-      </AppProvider>
-    </FirebaseProvider>
+    <AppProvider>
+      <PageClient />
+    </AppProvider>
   );
 }
